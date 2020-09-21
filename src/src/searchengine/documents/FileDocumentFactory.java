bb6672c53fc1da75a6f5f1c02844de6946e812c9
mattszeto <ms8288@gmail.com>
2020-09-21 10:30:17 -0700

    Initial commit

---
+++ src/src/searchengine/documents/FileDocumentFactory.java
@@ -0,0 +1,7 @@
+package src.searchengine.documents;
+
+import java.nio.file.Path;
+
+public interface FileDocumentFactory {
+	FileDocument createFileDocument(Path absoluteFilePath, int documentId);
+}
